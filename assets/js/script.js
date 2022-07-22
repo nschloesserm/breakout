0345ce0626a80dae600dd78c52985d946f30d5eb
nschloesserm <nicholasmckennaart@gmail.com>
2022-07-22 16:45:30 +0000

    add paddle and movement to javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,7 +13,7 @@ let ball = {
     dy: -speed + 1,
     radius: 7,
     draw: function() {
-        ctx.beginPath()
+        ctx.beginPath();
         ctx.fillStyle = '#61abc0';
         ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, true);
         ctx.closePath();
@@ -21,9 +21,31 @@ let ball = {
     }
 }
 
+let paddle = {
+    height: 12,
+    width: 100,
+    x: canvas.width/2 - 50,
+    y: canvas.height - 30,
+    draw: function() {
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, this.width, this.height);
+        ctx.fillStyle = '#61abc0';
+        ctx.closePath();
+        ctx.fill();
+    }
+}
+
+window.addEventListener('keydown', (event) => {
+    switch (event.key) {
+            case 'ArrowLeft' : paddle.x -= 15; break;
+            case 'ArrowRight' : paddle.x += 15; break;
+    }
+})
+
 function play() {
     ctx.clearRect(0,0,canvas.width,canvas.height)
     ball.draw();
+    paddle.draw();
 
     ball.x += ball.dx;
     ball.y += ball.dy;
@@ -32,12 +54,23 @@ function play() {
         ball.dx *= -1;
     }
 
-    if (ball.y + ball.radius > canvas.height || ball.y - ball.radius < 0) {
+    if (ball.y - ball.radius < 0) {
         ball.dy *= -1;
+    }
+
+    if (ball.y + ball.radius > canvas.height) {
+        ball.x = canvas.width/2;
+        ball.y = canvas.height - 100;
+        ball.dy = -speed + 1;
+    }
+
+    if (ball.x >= paddle.x && ball.x <= paddle.x + paddle.width && ball.y + ball.radius >= canvas.height- 12 - paddle.height) {
+        ball.dy *= -1.08;
     }
 
     requestAnimationFrame(play);
 }
+
 function startGame() {
     play();
 
